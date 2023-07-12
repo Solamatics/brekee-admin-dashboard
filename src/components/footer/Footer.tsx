d42b60bc64fce1efa4bbab754757aa1201c17194
Olusola Afikode <afikodeolaitan@gmail.com>
2023-07-12 17:35:55 +0100

    implement dashboard layout

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './footer.scss'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>bekree admin</span>
+      <span>@Bekreee Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
